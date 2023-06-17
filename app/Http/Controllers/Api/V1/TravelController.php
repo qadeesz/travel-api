87fb5fea68a76ae13d4c7877611093ecb7d50667
Qadees Javed <qadeesjaved9098@gmail.com>
2023-06-17 10:07:39 -0700

    Public Endpoint with Pagination and Tests

---
+++ app/Http/Controllers/Api/V1/TravelController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Models\Travel;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\TravelResource;
+
+class TravelController extends Controller
+{
+    function index() {
+
+        $travels = Travel::where("is_public", true)->paginate();
+        
+        return TravelResource::collection($travels);
+    }
+}
